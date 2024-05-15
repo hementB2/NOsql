7f95cd4649c90faa1230daf1c732201790bdaa5c
hementB2 <153564981+hementB2@users.noreply.github.com>
2024-05-15 07:46:22 -0400

    adding code

---
+++ 18-NoSQL/01-Activities/06-Stu_Update-Delete/Unsolved/server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const mongodb = require('mongodb').MongoClient;
+// We import the ObjectId class from mongodb
+const { MongoClient, ObjectId } = require('mongodb');
+
+const app = express();
+const port = 3001;
+
+const connectionStringURI = `mongodb://127.0.0.1:27017`;
+
+const client = new MongoClient(connectionStringURI);
+
+let db;
+
+const dbName = 'bookCollectionDB';
+
+client.connect()
+  .then(() => {
+    console.log('Connected successfully to MongoDB');
+    db = client.db(dbName);
+
+    app.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Mongo connection error: ', err.message);
+  });
+
+app.use(express.json());
+
+app.post('/books', (req, res) => {
+  // The title and author will be provided by the request body
+  db.collection('bookCollection').insertOne(
+    { title: req.body.title, author: req.body.author }
+  )
+    .then(results => res.json(results))
+    .catch(err => {
+      if (err) throw err;
+    });
+});
+
+app.get('/books', (req, res) => {
+  db.collection('bookCollection')
+    .find({})
+    .toArray()
+    .then(results => res.json(results))
+    .catch(err => {
+      if (err) throw err;
+    });
+});
+
+// TODO: Add Delete route that uses a filter to delete a single document by id
+// To delete a document, we need to convert the string id parameter to an ObjectId
+app.delete('/books/:id', (req, res) => {
+
+  // Wrap the id in the ObjectId class to instantiate a new instance
+  const bookId = new ObjectId(req.params.id);
+
+  // Use deleteOne() to delete one object
+  db.collection('bookCollection').deleteOne(
+    // This is the filter. We delete only the document that matches the _id provided in the specified parameter.
+    { _id: bookId }
+  )
+    .then(results => {
+      console.log(results);
+      res.send(
+        results.deletedCount ? 'Document deleted' : 'No document found!'
+      );
+    })
+    .catch(err => {
+      if (err) throw err;
+    });
+});
